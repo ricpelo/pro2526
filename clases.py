a1c77c5fbf2ea4cab22d9d0aa916f2f340bc6943
Ricardo Pérez <ricpelo@gmail.com>
2026-03-06 19:32:47 +0100

    Viernes 6 de marzo

--- clases.py
+++ clases.py
@@ -1,3 +1,5 @@
+from math import gcd
+
 
 class Pareja:
     def __init__(self, x, y) -> None:
@@ -15,6 +17,12 @@ class Pareja:
     def __hash__(self):
         return hash((self.select(0), self.select(1)))
 
+    def __repr__(self):
+        return f'Pareja({self.select(0)!r}, {self.select(1)!r})'
+
+    def __str__(self):
+        return f'<{self.select(0)}, {self.select(1)}>'
+
     def select(self, i: int):
         if i == 0:
             return self.x
@@ -26,7 +34,8 @@ class Pareja:
 
 class Racional:
     def __init__(self, num: int, den: int) -> None:
-        self.pareja = Pareja(num, den)
+        mcd = gcd(num, den)
+        self.pareja = Pareja(num // mcd, den // mcd)
         
     def __eq__(self, otro):
         if type(self) != type(otro):
@@ -36,6 +45,12 @@ class Racional:
     def __hash__(self):
         return hash((self.numer(), self.denom()))
     
+    def __repr__(self):
+        return f'Racional({self.numer()!r}, {self.denom()!r})'
+    
+    def __str__(self):
+        return f'{self.numer()}/{self.denom()}'
+        
     def numer(self):
         return self.pareja.select(0)
     
@@ -44,16 +59,35 @@ class Racional:
     
     
 class Deposito:
-    def __init__(self, fondos):
+    def __init__(self, fondos, movimientos = None):
         self.fondos = fondos
-        
+        if movimientos is None:
+            self.movimientos: list[tuple[str, float]] = [('APERTURA', fondos)]
+        else:
+            self.movimientos = movimientos
+    
+    def __eq__(self, otro):
+        if type(self) != type(otro):
+            return NotImplemented
+        return self.movimientos == otro.movimientos
+    
+    def __repr__(self) -> str:
+        return f'Deposito({self.saldo()!r}, {self.movimientos!r})'
+    
+    def __str__(self):
+        mov = len(self.movimientos)
+        sal = self.saldo()
+        return f'Esta cuenta tiene {mov} movimientos y un saldo de {sal:.2f} €.'
+    
     def ingresar(self, cantidad):
         self.fondos += cantidad
+        self.movimientos.append(('INGRESO', cantidad))
         
     def retirar(self, cantidad):
         if cantidad > self.fondos:
             raise ValueError('Fondos insuficientes.')
         self.fondos -= cantidad
+        self.movimientos.append(('REINTEGRO', -cantidad))
         
     def saldo(self):
         return self.fondos
--- persona.py
+++ persona.py
@@ -1,15 +1,75 @@
 
 
 class Persona:
+    """
+    Inv:
+        - self.get_nombre() != ''
+        - self.get_apellidos() != ''
+        - self.get_dni() != ''
+    """
+        
     def __init__(self, dni, nombre, apellidos):
-        self.dni = dni
-        self.nombre = nombre
-        self.apellidos = apellidos
+        self.__dni = dni
+        self.set_nombre(nombre)
+        self.__apellidos = apellidos
+        self.__invariante()
 
     def __eq__(self, otro):
         if type(self) != type(otro):
             return NotImplemented
-        return self.dni == otro.dni
+        return self.__dni == otro.__dni
     
     def __hash__(self):
-        return hash(self.dni)
+        return hash(self.__dni)
+
+    def __repr__(self):
+        dni = self.__dni
+        nom = self.__nombre
+        ape = self.__apellidos
+        return f'Persona({dni!r}, {nom!r}, {ape!r})'
+
+    def __str__(self):
+        dni = self.__dni
+        nom = self.__nombre
+        ape = self.__apellidos
+        return f'Esta persona tiene el DNI {dni} y se llama {nom} {ape}.'
+
+    def __invariante(self):
+        inv = self.get_nombre() != '' and
+              self.get_apellidos() != '' and
+              self.get_dni() != ''
+        assert inv, "Violación del invariante."
+
+    def __comprobar_vacio(self, valor):
+        if valor == '':
+            raise ValueError('El valor no puede ser vacío.')
+
+    def get_dni(self):
+        return self.__dni
+    
+    def get_nombre(self):
+        return self.__nombre
+    
+    def get_apellidos(self):
+        return self.__apellidos
+    
+    def get_nombre_completo(self):
+        return f'{self.__nombre} {self.__apellidos}'
+    
+    def __set_dni(self, dni):
+        self.__comprobar_vacio(dni)
+        self.__dni = dni
+        assert self.get_dni() == dni
+        self.__invariante()
+
+    def set_nombre(self, nombre):
+        self.__comprobar_vacio(nombre)
+        self.__nombre = nombre
+        assert self.get_nombre() == nombre
+        self.__invariante()
+        
+    def set_apellidos(self, apellidos):
+        self.__comprobar_vacio(apellidos)
+        self.__apellidos = apellidos
+        assert self.get_apellidos() == apellidos
+        self.__invariante()
