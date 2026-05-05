6ed4f99d1672685b04faa8d364523c233e72242f
Ricardo Pérez <ricpelo@gmail.com>
2026-05-05 12:40:54 +0200

    Corrige erratas

--- tienda/articulo.py
+++ tienda/articulo.py
@@ -7,28 +7,28 @@ class Articulo:
     def __eq__(self, otro) -> bool:
         if type(self) != type(otro):
             return NotImplemented
-        return self.codigo == self.otro
-    
-    def __hash__(self) -> str:
+        return self.codigo == otro.codigo
+
+    def __hash__(self) -> int:
         return hash(self.codigo)
 
     @property
     def codigo(self) -> str:
         return self.__codigo
-    
+
     @property
     def denominacion(self) -> str:
         return self.__denominacion
-    
+
     @property
     def precio(self) -> float:
         return self.__precio
-    
+
     def __set_codigo(self, codigo):
         self.__codigo = codigo
-    
+
     def set_denominacion(self, denominacion):
         self.__denominacion = denominacion
-        
+
     def set_precio(self, precio):
         self.__precio = precio
--- tienda/cliente.py
+++ tienda/cliente.py
@@ -7,28 +7,28 @@ class Cliente:
     def __eq__(self, otro) -> bool:
         if type(self) != type(otro):
             return NotImplemented
-        return self.dni == self.otro
-    
-    def __hash__(self) -> str:
+        return self.dni == otro.dni
+
+    def __hash__(self) -> int:
         return hash(self.dni)
 
     @property
     def dni(self) -> str:
         return self.__dni
-    
+
     @property
     def nombre(self) -> str:
         return self.__nombre
-    
+
     @property
     def apellidos(self) -> str:
         return self.__apellidos
-    
+
     def __set_dni(self, dni):
         self.__dni = dni
-    
+
     def set_nombre(self, nombre):
         self.__nombre = nombre
-        
+
     def set_apellidos(self, apellidos):
         self.__apellidos = apellidos
